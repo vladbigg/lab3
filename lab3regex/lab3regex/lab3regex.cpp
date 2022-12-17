e162a1a872b04f154408e2b4b737be1bf32eabc9
Ь Во <malahov.03.03@mail.com>
2022-12-17 22:55:03 +0300

    dikiyborov

---
+++ lab3regex/lab3regex/lab3regex.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <regex>
+
+using namespace std;
+
+int main()
+{
+	ifstream f("test.txt");
+	string line;
+	smatch match;
+
+	const auto r = regex(R"((z).{3}(z))");
+	while (getline(f, line))
+	{
+		regex_search(line, match, r);
+		if (!match.empty()) cout << line << "\n\n";
+	}
+}
